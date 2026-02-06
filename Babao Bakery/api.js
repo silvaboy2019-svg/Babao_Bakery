e2d2a008ddfa803d6afc3964118990d4107c4f94
Mohammed Abdalla ali <s.ilvaboy2019@gmail.com>
2026-02-06 19:54:04 +0100

    Add files via upload

---
+++ Babao Bakery/api.js
@@ -0,0 +1,4 @@
+function showMessage(event) {
+    event.preventDefault(); // Prevents the page from reloading
+    alert("Thank you! Babao Bakery has received your message and will contact you soon.");
+}
